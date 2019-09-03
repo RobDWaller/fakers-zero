4604863029fdd87f4ce8be92f8e0ac01b1465db9
RobDWaller <rdwaller1984@googlemail.com>
2019-09-03 06:56:24 +0100

    Created initial slim test route.

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write("<h1>Hello Fakers!</h1>");
+    return $response;
+});
+
+$app->run();
