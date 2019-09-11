62b615888267d9c894740ce1ff1ff8c0fb2c5350
RobDWaller <rdwaller1984@googlemail.com>
2019-09-11 06:51:05 +0100

    Capitalised views directory in services file, this is neater.

--- src/services.php
+++ src/services.php
@@ -4,6 +4,6 @@ use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 
 $container->set('view', function () {
-    $twigLoader = new FilesystemLoader(__DIR__ . '/views');
+    $twigLoader = new FilesystemLoader(__DIR__ . '/Views');
     return new Environment($twigLoader, ['cache' => __DIR__ . '/cache']);
 });
