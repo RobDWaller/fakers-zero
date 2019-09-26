6ac6a9e881252b7996eaff4712f8bc86790ed373
RobDWaller <rdwaller1984@googlemail.com>
2019-09-26 07:20:07 +0100

    Added services for Uri Helper, SlimSession and TwitterOAuth.

--- src/services.php
+++ src/services.php
@@ -7,3 +7,18 @@ $container->set('view', function () {
     $twigLoader = new FilesystemLoader(__DIR__ . '/Views');
     return new Environment($twigLoader, ['cache' => __DIR__ . '/cache']);
 });
+
+$container->set('session', function () {
+    return new SlimSession\Helper();
+});
+
+$container->set('uri', function () {
+    return new App\Helper\Uri;
+});
+
+$container->set('twitter_oauth', function () {
+    return new App\Twitter\TwitterOAuth(
+        new App\Twitter\OAuth\Auth(getenv('TWITTER_KEY'), getenv('TWITTER_SECRET')),
+        new App\Twitter\OAuth\Factory()
+    );
+});
