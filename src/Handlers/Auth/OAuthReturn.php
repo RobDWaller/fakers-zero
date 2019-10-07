518e9bc0038a6ca9015c40ba20a5c841f775db34
RobDWaller <rdwaller1984@googlemail.com>
2019-10-07 07:31:07 +0100

    Updated OAuthReturn handler so it will create a new user and update an existing user in the database.

--- src/Handlers/Auth/OAuthReturn.php
+++ src/Handlers/Auth/OAuthReturn.php
@@ -8,6 +8,8 @@ use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use App\Aggregates\Twitter;
+use App\Aggregates\User;
+use App\Helper\Uri;
 use Slim\Psr7\Factory\ResponseFactory;
 use Exception;
 
@@ -15,26 +17,55 @@ class OAuthReturn implements RequestHandlerInterface
 {
     private $twitter;
 
-    public function __construct(Twitter $twitter)
+    private $user;
+
+    private $url;
+
+    public function __construct(Twitter $twitter, User $user, Uri $uri)
     {
         $this->twitter = $twitter;
+
+        $this->user = $user;
+
+        $this->uri = $uri;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $factory = new ResponseFactory();
+
+        $url = $this->uri->build([
+            'scheme' => 'http',
+            'host' => 'fakers.test',
+            'path' => '/dashboard'
+        ]);
         
         try {
             $tokens = $this->twitter->getAccessTokens($request);
             
-            $response = $factory->createResponse(200, 'Ok');
-            $response = $response->withAddedHeader('Content-Type', 'application/json');
-            $response->getBody()->write((string) json_encode($tokens));
-            return $response;
+            if (!$this->user->exists((int) $tokens['user_id'])) {
+                $this->user->add($tokens);
+                
+                $response = $factory->createResponse(303, 'See Other');
+                return $response->withAddedHeader('Location', $url);
+            }
+            
+            $this->user->update($tokens);
+
+            $response = $factory->createResponse(303, 'See Other');
+            return $response->withAddedHeader('Location', $url);
+            
         } catch (Exception $e) {
             $response = $factory->createResponse(401, 'Unauthorised');
             $response = $response->withAddedHeader('Content-Type', 'application/json');
-            $response->getBody()->write((string) json_encode(['message' => 'Twitter Authorisation Failed']));
+            $response->getBody()->write(
+                (string) json_encode(
+                    [
+                        'message' => 'Twitter Authorisation Failed',
+                        'data' => $e->getMessage()
+                    ]
+                )
+            );
             return $response;
         }
     }
