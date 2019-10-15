941ee94e62545e95632d5cdb032aecc5b5af85d0
RobDWaller <rdwaller1984@googlemail.com>
2019-10-15 07:23:15 +0100

    Amended Uri class and tests to work with new Environment class instead of environment config array.

--- src/Helper/Uri.php
+++ src/Helper/Uri.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace App\Helper;
 
+use App\Helper\Environment;
 use Sabre\Uri as SabreUri;
 
 class Uri
 {
     private $environment;
 
-    public function __construct($environment)
+    public function __construct(Environment $environment)
     {
         $this->environment = $environment;
     }
@@ -18,39 +19,9 @@ class Uri
     public function build(string $path): string
     {
         return SabreUri\build([
-            "scheme" => $this->getScheme(),
-            "host" => $this->getHost(),
+            "scheme" => $this->environment->getScheme(),
+            "host" => $this->environment->getHost(),
             "path" => $path
         ]);
-    }
-
-    public function getHost(): string
-    {
-        if (isset($this->environment['HTTP_HOST'])) {
-            return $this->environment['HTTP_HOST'];
-        }
-
-        if (isset($this->environment['SERVER_NAME'])) {
-            return $this->environment['SERVER_NAME'];
-        }
-
-        return '';
-    }
-
-    public function getScheme(): string
-    {
-        return $this->isHttps() || $this->isPort443() ? 'https' : 'http';
-    }
-
-    private function isHttps(): bool
-    {
-        return !empty($this->environment['HTTPS'])
-        && $this->environment['HTTPS'] !== 'off';
-    }
-
-    private function isPort443(): bool
-    {
-        return isset($this->environment['SERVER_PORT'])
-        && $this->environment['SERVER_PORT'] === '443';
     }
 }
--- tests/Unit/Helper/UriTest.php
+++ tests/Unit/Helper/UriTest.php
@@ -4,13 +4,15 @@ namespace Tests\Unit\Helper;
 
 use PHPUnit\Framework\TestCase;
 use App\Helper\Uri;
-use ReflectionMethod;
+use App\Helper\Environment;
 
 class UriTest extends TestCase
 {
     public function testUri()
     {
-        $environment = [];
+        $config = [];
+
+        $environment = new Environment($config);
 
         $uri = new Uri($environment);
 
@@ -19,147 +21,14 @@ class UriTest extends TestCase
 
     public function testBuild()
     {
-        $environment = ['HTTP_HOST' => 'fakers.com'];
+        $config = ['HTTP_HOST' => 'fakers.com'];
+
+        $environment = new Environment($config);
 
         $uri = new Uri($environment);
 
         $url = $uri->build('/authenticate');
 
         $this->assertSame('http://fakers.com/authenticate', $url);
-    }
-
-    public function testGetHost()
-    {
-        $environment = ['HTTP_HOST' => 'fakers.com'];
-
-        $uri = new Uri($environment);
-
-        $this->assertSame($uri->getHost(), 'fakers.com');
-    }
-
-    public function testGetHostWithServerName()
-    {
-        $environment = ['SERVER_NAME' => 'fakers.com'];
-
-        $uri = new Uri($environment);
-
-        $this->assertSame($uri->getHost(), 'fakers.com');
-    }
-
-    public function testGetHostNone()
-    {
-        $environment = [];
-
-        $uri = new Uri($environment);
-
-        $this->assertSame($uri->getHost(), '');
-    }
-
-    public function testGetHostWithBoth()
-    {
-        $environment = [
-            'HTTP_HOST' => 'fakers.com',
-            'SERVER_NAME' => 'two.fakers.com'
-        ];
-
-        $uri = new Uri($environment);
-
-        $this->assertSame($uri->getHost(), 'fakers.com');
-    }
-
-    public function testGetScheme()
-    {
-        $environment = [];
-
-        $uri = new Uri($environment);
-
-        $this->assertSame($uri->getScheme(), 'http');
-    }
-
-    public function testGetSchemeSecure()
-    {
-        $environment = ['HTTPS' => 'on'];
-
-        $uri = new Uri($environment);
-
-        $this->assertSame($uri->getScheme(), 'https');
-    }
-
-    public function testGetSchemeSecurePort()
-    {
-        $environment = ['SERVER_PORT' => "443"];
-
-        $uri = new Uri($environment);
-
-        $this->assertSame($uri->getScheme(), 'https');
-    }
-
-    public function testGetSchemeOff()
-    {
-        $environment = ['HTTPS' => "off"];
-
-        $uri = new Uri($environment);
-
-        $this->assertSame($uri->getScheme(), 'http');
-    }
-
-    public function testIsHttps()
-    {
-        $environment = ['HTTPS' => "on"];
-
-        $uri = new Uri($environment);
-
-        $method = new ReflectionMethod(Uri::class, 'isHttps');
-        $method->setAccessible(true);
-
-        $this->assertTrue($method->invoke($uri));
-    }
-
-    public function testIsHttpsFalse()
-    {
-        $environment = ['HTTPS' => "off"];
-
-        $uri = new Uri($environment);
-
-        $method = new ReflectionMethod(Uri::class, 'isHttps');
-        $method->setAccessible(true);
-
-        $this->assertFalse($method->invoke($uri));
-    }
-
-    public function testIsHttpsNotSet()
-    {
-        $environment = [];
-
-        $uri = new Uri($environment);
-
-        $method = new ReflectionMethod(Uri::class, 'isHttps');
-        $method->setAccessible(true);
-
-        $this->assertFalse($method->invoke($uri));
-    }
-
-    public function testIsPort443False()
-    {
-        $environment = ['SERVER_PORT' => "80"];
-
-        $uri = new Uri($environment);
-
-        $method = new ReflectionMethod(Uri::class, 'isPort443');
-        $method->setAccessible(true);
-
-        $this->assertFalse($method->invoke($uri));
-    }
-
-    public function testIsPort443NotSet()
-    {
-        $environment = [];
-
-        $uri = new Uri($environment);
-
-        $method = new ReflectionMethod(Uri::class, 'isPort443');
-        $method->setAccessible(true);
-
-        $this->assertFalse($method->invoke($uri));
     }
 }
