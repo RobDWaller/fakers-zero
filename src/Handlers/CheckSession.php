4ba3da1096a7fd8aab882cd41ea956893ea4fee6
RobDWaller <rdwaller1984@googlemail.com>
2019-10-16 07:26:46 +0100

    Created CheckSession handler class, this confirms a user has a valid session and if so returns an access token.

---
+++ src/Handlers/CheckSession.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Handlers;
+
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Psr7\Factory\ResponseFactory;
+use App\Aggregates\User;
+use App\Helper\Token;
+
+class CheckSession implements RequestHandlerInterface
+{
+    private $user;
+
+    private $token;
+
+    public function __construct(User $user, Token $token)
+    {
+        $this->user = $user;
+
+        $this->token = $token;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        if ($this->user->checkSession()) {
+            try {
+                $token = $this->token->make($this->user->getSessionUserId());
+
+                return $this->success(200, 'Ok', 'Access Token created successfully.', ['access_token' => $token]);
+            } catch (\Exception $e) {
+                return $this->error(500, 'Internal Server Error.', 'Something went wrong with authorisation.');
+            }
+        }
+
+        return $this->error(401, 'Unathorised', 'Please login to aquire an access token.');
+    }
+}
